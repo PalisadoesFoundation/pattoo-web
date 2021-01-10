49d82bbf8c5abd06b56b223852ccf9ea824c6047
FireFoxHD <khamalipowell@gmail.com>
2021-01-10 01:50:10 -0500

    ui changes

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+/* React Imports */
+import React from "react";
+
+function Card() {
+    return (
+       <div className="flex flex-col items-center rounded-md m-4 p-4 w-64 h-64 border shadow-lg">
+            <p className="items-start font-bold text-xl text-blue-900">Recently Updated</p>
+            <div className="px-6 py-4 text-white font-bold">
+                <div className="bg-blue-500 m-2 w-48 p-2 rounded-md text-center">Favorites</div>
+                <div className="bg-blue-500 m-2 w-48 p-2 rounded-md text-center">Charts</div>
+                <div className="bg-blue-500 m-2 w-48 p-2 rounded-md text-center">Agents</div>
+            </div>
+       </div>
+    );
+}
+
+export default Card;
---
+++ src/components/Graphcard/Graphcard.test.js
@@ -0,0 +1,4 @@
+import Graphcard from "./Graphcard";
+
+// Unittest stub until the Graph Card is implemented
+test("Snapshot test of the Graph Card item", () => {});
