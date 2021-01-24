d308d5815d5f3088a01ef8eaedc16b628a588502
DangaRanga <jasonblue1626@gmail.com>
2021-01-23 20:10:32 -0500

    Fixed unused vars

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import Login from "./components/Login/Login";
 
 // Catches and logs graphql errors
-const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+const errorLink = onError(({ graphqlErrors }) => {
   if (graphqlErrors) {
-    graphqlErrors.map(({ message, location, path }) => {
+    graphqlErrors.map(({ message }) => {
       alert(`A graphQl error occurred ${message}`);
     });
   }
