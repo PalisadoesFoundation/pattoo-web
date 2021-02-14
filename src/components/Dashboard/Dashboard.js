b62c60adc2726331417a51d432e198a32fb9a1a0
DangaRanga <jasonblue1626@gmail.com>
2021-02-13 21:06:34 -0500

    Slight modifications to routing

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,32 +1,41 @@
 /* React Imports */
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 /* Module imports */
-import useBreakpoint from "../../hooks/useBreakpoint"
-import Transition from "../../hooks/Transition"
+import useBreakpoint from "../../hooks/useBreakpoint";
+import Transition from "../../hooks/Transition";
 import Sidebar from "../Sidebar/Sidebar";
 import Header from "../Header/Header";
 import DataCard from "../DataCard/DataCard";
 import StatCard from "../StatCard/StatCard";
-import GraphCard from "../GraphCard/GraphCard";
+import Graphcard from "../Graphcard/Graphcard";
 
 /* Importing Styles*/
 import "../../styles/main.css";
 
 function Dashboard() {
   //Dashboard Page
-  const[isClosed, setClosed] = useState(false);
+  const [isClosed, setClosed] = useState(false);
   const isStatic = useBreakpoint("md");
 
   return (
-
     //======================== Desktop or laptop ========================//
     <div className="flex bg-gray-200">
-      <Transition show={(isStatic || !isClosed)} enter="transition-all duration-200" enterFrom="-ml-64" enterTo="ml-0" leave="transition-all duration-200" leaveTo="-ml-64">
-        <Sidebar isStatic={isStatic} isClosed={isClosed} setClosed={setClosed} />
+      <Transition
+        show={isStatic || !isClosed}
+        enter="transition-all duration-200"
+        enterFrom="-ml-64"
+        enterTo="ml-0"
+        leave="transition-all duration-200"
+        leaveTo="-ml-64"
+      >
+        <Sidebar
+          isStatic={isStatic}
+          isClosed={isClosed}
+          setClosed={setClosed}
+        />
       </Transition>
 
-    
       <Transition
         appear={true}
         show={!isStatic && !isClosed}
@@ -37,39 +46,59 @@ function Dashboard() {
         leaveFrom="opacity-5"
         leaveTo="opacity-0"
       >
-        <div className="fixed inset-0 bg-black opacity-0"/>
+        <div className="fixed inset-0 bg-black opacity-0" />
       </Transition>
 
-        <main id="main-content" className="flex flex-col flex-grow min-h-screen min-w-screen">
+      <main
+        id="main-content"
+        className="flex flex-col flex-grow min-h-screen min-w-screen"
+      >
+        <Header
+          isStatic={isStatic}
+          setClosed={setClosed}
+          isClosed={isClosed}
+          label={"Dashboard"}
+          name={"Jason Gayle"}
+          title={"Python Developer"}
+        />
 
-          <Header isStatic={isStatic} setClosed={setClosed} isClosed={isClosed} label={"Dashboard"} name={"Jason Gayle"} title={"Python Developer"} />
-        
-              <div>
-                <div className="flex items-center flex-wrap">
-                  <DataCard title={"Files Injested"} value={"296"} lastChecked={"18 hours ago"} timespan={"Daily"}/>
-                  <DataCard title={"Tracked Datapoints"} value={"150"} lastChecked={"18 hours ago"} timespan={"Daily"}/>
-                  <DataCard title={"Active Agents"} value={"25"} lastChecked={"18 hours ago"} timespan={"Daily"}/>
-                  <DataCard title={"Running Processes"} value={"15"} lastChecked={"18 hours ago"} timespan={"Daily"}/>
-                </div>
+        <div>
+          <div className="flex items-center flex-wrap">
+            <DataCard
+              title={"Files Injested"}
+              value={"296"}
+              lastChecked={"18 hours ago"}
+              timespan={"Daily"}
+            />
+            <DataCard
+              title={"Tracked Datapoints"}
+              value={"150"}
+              lastChecked={"18 hours ago"}
+              timespan={"Daily"}
+            />
+            <DataCard
+              title={"Active Agents"}
+              value={"25"}
+              lastChecked={"18 hours ago"}
+              timespan={"Daily"}
+            />
+            <DataCard
+              title={"Running Processes"}
+              value={"15"}
+              lastChecked={"18 hours ago"}
+              timespan={"Daily"}
+            />
+          </div>
 
-                <div className="flex items-center flex-wrap">
-                  <StatCard recentItems={["Favorites", "Charts", "Agents"]}/>
-                  <GraphCard />
-                </div>
-
-
-
-
-              </div>
-
-        </main>
-      
+          <div className="flex items-center flex-wrap">
+            <StatCard recentItems={["Favorites", "Charts", "Agents"]} />
+            <Graphcard />
+          </div>
+        </div>
+      </main>
     </div>
 
-
-     //=========================== Mobile ==============================//
-
-
+    //=========================== Mobile ==============================//
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,59 @@
 /* React Imports */
 import React from "react";
-import PropTypes from 'prop-types';
-import {IconContext} from "react-icons";
-import {MdMenu} from "react-icons/md";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { IconContext } from "react-icons";
+import { MdMenu } from "react-icons/md";
 import { IoIosLogOut } from "react-icons/io";
 
 import "../../styles/main.css";
 
 //replace navigtion links when pages are created
 
-function Header({label, name, title, setClosed, isClosed, isStatic}) {
+function Header({ label, name, title, setClosed, isClosed, isStatic }) {
   return (
-    <header className="flex justify-between	w-full px-6 items-center h-16 bg-white text-white shadow-lg z-10">{/*text color here affects the icon color*/}
-      
-      <IconContext.Provider  value={{ size: '24px'}}>
-      {(!isStatic && isClosed) && ( 
-        <button title={"Open menu"} onClick={() => setClosed(false)} className="p-2 bg-gray-500 rounded-md" aria-label={"Open menu"}>
-          <MdMenu/>
-        </button>
-      )}
+    <header className="flex justify-between	w-full px-6 items-center h-16 bg-white text-white shadow-lg z-10">
+      {/*text color here affects the icon color*/}
+
+      <IconContext.Provider value={{ size: "24px" }}>
+        {!isStatic && isClosed && (
+          <button
+            title={"Open menu"}
+            onClick={() => setClosed(false)}
+            className="p-2 bg-gray-500 rounded-md"
+            aria-label={"Open menu"}
+          >
+            <MdMenu />
+          </button>
+        )}
       </IconContext.Provider>
 
-        <div className="inline-flex px-6 text-gray-800 text-4xl font-bold ">
-          {label}
-        </div>
+      <div className="inline-flex px-6 text-gray-800 text-4xl font-bold ">
+        {label}
+      </div>
 
-        <div className="flex items-center h-full">
-          <div className="flex-shrink-0 h-10 w-10">
-            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-10 w-10 rounded-full" alt="profile"/>
-          </div>
-          <div className={`ml-2 flex-shrink ${isStatic ? '' : 'hidden'}`}>
-              <p className="text-sm leading-5 font-medium text-gray-900">{name}</p>
-              <p className="text-sm leading-5 text-gray-500">{title}</p>
-          </div>
-          <div>
-            <a href="index.html">
-              <button className="bg-gray-800 text-white border font-bold p-2 rounded-md ml-4 shadow">
-              <IconContext.Provider  value={{ size: '24px'}}>
+      <div className="flex items-center h-full">
+        <div className="flex-shrink-0 h-10 w-10">
+          <img
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            className="h-10 w-10 rounded-full"
+            alt="profile"
+          />
+        </div>
+        <div className={`ml-2 flex-shrink ${isStatic ? "" : "hidden"}`}>
+          <p className="text-sm leading-5 font-medium text-gray-900">{name}</p>
+          <p className="text-sm leading-5 text-gray-500">{title}</p>
+        </div>
+        <div>
+          <Link to="/login">
+            <button className="bg-gray-800 text-white border font-bold p-2 rounded-md ml-4 shadow">
+              <IconContext.Provider value={{ size: "24px" }}>
                 <IoIosLogOut />
               </IconContext.Provider>
-              </button>
-            </a>
-          </div>
+            </button>
+          </Link>
         </div>
+      </div>
     </header>
   );
 }
@@ -60,5 +71,5 @@ Header.propTypes = {
 
 //            TODO
 //add hamburger option for mobile
-//add pattoo logo 
+//add pattoo logo
 //add logout button
